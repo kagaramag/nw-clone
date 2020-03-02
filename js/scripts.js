4c3702c28bed21362ba35ae10829a2122b7068a8
kagaramag <kagaramag@gmail.com>
2020-03-03 00:02:58 +0200

    optimize html for bootstrap framework

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,7 @@ window.onscroll = function() {
 };
 
 // Get elements by IDs
-var header = document.getElementById('header');
-var navbar = document.getElementById('navbar');
-var timer = document.getElementById('timer');
-var logo = document.getElementById('image-logo');
-var nabbuttons = document.getElementById('nabbuttons');
-var switchbutton = document.getElementById('switchbutton');
-var hideOnSmall = document.getElementsByClassName('hideOnSmall');
+var fixedHeader = document.getElementById('fixed-header');
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
@@ -20,29 +14,8 @@ var sticky = header.offsetTop;
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky && window.pageYOffset > 220) {
-    header.classList.add('sticky');
-    navbar.classList.add('d-none');
-    timer.classList.add('d-none');
-    switchbutton.classList.remove('d-none');
-    logo.classList.add('logo-on-small');
-    nabbuttons.classList.remove('col-lg-3');
-    nabbuttons.classList.add('col-lg-6');
+    fixedHeader.classList.remove('d-none');
   } else {
-    header.classList.remove('sticky');
-    navbar.classList.remove('d-none');
-    timer.classList.remove('d-none');
-    switchbutton.classList.add('d-none');
-    logo.classList.remove('logo-on-small');
-    nabbuttons.classList.add('col-lg-3');
-    nabbuttons.classList.remove('col-lg-6');
+    fixedHeader.classList.add('d-none');
   }
-  // if (window.innerWidth <= 767) {
-  //   hideOnSmall.style.display = 'none';
-  // }
-  // if (window.innerWidth >= 767 && window.innerWidth <= 991) {
-  //   hideOnSmall.style.display = 'none';
-  // }
-  // if (window.innerWidth >= 991) {
-  //   hideOnSmall.style.display = 'none';
-  // }
 }
